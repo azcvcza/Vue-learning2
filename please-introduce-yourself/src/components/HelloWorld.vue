240290a18216e529494be255e7c9d01169a697e6
韦粤信 <283940802@qq.com>
2018-12-06 00:39:33 +0800

    feature: before buy a domain

---
+++ please-introduce-yourself/src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+	<form @submit="addMessage">
+		<div class="form-group">
+			<input
+				class="form-control"
+				v-model="newMessage.title"
+				maxlength="40"
+				autofocus
+				placeholder="Pleaseintroduce yourself :)"
+			>
+		</div>
+		<div class="form-group">
+			<textarea
+				class="form-control"
+				v-model="newMessage.text"
+				placeholder="Leave your message!"
+				rows="3"
+			></textarea>
+		</div>
+	</form>
+</template>
+
+<script>
+	export default {
+		name: "HelloWorld",
+		data() {
+			return {
+				newMessage: {
+					title: "",
+					text: "",
+					timestamp: null
+				}
+			};
+		}
+	};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+	font-weight: normal;
+}
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #42b983;
+}
+</style>
